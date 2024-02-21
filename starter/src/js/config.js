5620e8011f49387411dc866478ad76daa659afd2
jshy0 <joshjhharris@gmail.com>
2024-02-21 14:55:15 +0000

    Initial Commit

---
+++ starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_KEY = 'c1736c4e-c797-4072-be2e-f7c7c765d11a';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2;
